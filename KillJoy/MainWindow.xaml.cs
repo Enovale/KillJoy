96dc4e5602ae4c18203e42c800b50cb514fa7179
ElijahZAwesome <>
2019-10-08 07:13:29 -0500

    tray icon

--- KillJoy/MainWindow.xaml.cs
+++ KillJoy/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -25,11 +27,35 @@ namespace KillJoy
         public HomePage homeObj;
         public SettingsPopup settingsObj;
 
+        private NotifyIcon notifier = new NotifyIcon();
+
         public MainWindow()
         {
             InitializeComponent();
             homeObj = new HomePage();
             this.MainFrame.Navigate(homeObj);
+
+            this.notifier.MouseDown += new System.Windows.Forms.MouseEventHandler(notifier_MouseDown);
+            this.notifier.Icon = KillJoy.Properties.Resources.A;
+            this.notifier.Visible = true;
+        }
+
+        protected override void OnGotFocus(RoutedEventArgs e)
+        {
+            this.UseGlowEffect = true;
+        }
+
+        protected override void OnContentRendered(EventArgs e)
+        {
+            base.OnContentRendered(e);
+            this.UseGlowEffect = true;
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+            this.UseGlowEffect = false;
         }
 
         private void MainWindow_DisplaySettings(object sender, RoutedEventArgs e)
@@ -40,6 +66,27 @@ namespace KillJoy
         private void MainWindow_DisplayHome(object sender, RoutedEventArgs e)
         {
             this.MainFrame.Navigate(homeObj);
+        }
+
+        void notifier_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left || e.Button == MouseButtons.Right)
+            {
+                //System.Windows.Controls.ContextMenu menu = (System.Windows.Controls.ContextMenu)this.FindResource("NotifierContextMenu");
+                //menu.IsOpen = true;
+                this.Show();
+                this.Focus();
+            }
+        }
+
+        private void Menu_Open(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Open");
+        }
+
+        private void Menu_Close(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Close");
         }
 
     }
